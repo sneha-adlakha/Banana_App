42b456aead46d109f7658a52c66ad599adcf688d
Sneha <sneha.adlakha@gmail.com>
2020-12-03 12:46:02 +0530

    input processing output

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-var buttonTranslate=document.querySelector("#btn-translate");
-buttonTranslate.addEventListener("click",function clickEventHandler() {
-        console.log("clicked")
-})
+var buttonTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+        
+function clickHandler()
+{
+        outputDiv.innerText = "hello" + txtInput.value;
+};
+buttonTranslate.addEventListener("click",clickHandler)
